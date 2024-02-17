870507a3f8565e1dbcf12374b5726c019e2586d6
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-02-17 22:07:42 +0000

    feat: implemented Sensor class having troubles with socket binding especially getPort and getAddress

--- sensor/ISensor.java
+++ sensor/ISensor.java
@@ -3,13 +3,15 @@ package sensor;
  * Updated on Feb 2023
  */
 import common.MessageInfo;
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 public interface ISensor {
-    /* sends N measurementes to the Field Unit*/
-    public void run(int N) throws InterruptedException;
+  /* sends N measurementes to the Field Unit*/
+  public void run(int N) throws InterruptedException, IOException;
 
     /* Send the message 'msg' to 'address' on port 'port' */
-    public void sendMessage(String address, int port, MessageInfo msg);
+    public void sendMessage(String address, int port, MessageInfo msg) throws IOException;
 
     /* Simulate one measurement */
     public float getMeasurement();
--- sensor/Sensor.java
+++ sensor/Sensor.java
@@ -1,4 +1,5 @@
 package sensor;
+
 /*
  * Updated on Feb 2023
  */
@@ -8,80 +9,99 @@ import java.io.IOException;
 import java.net.*;
 import java.util.Random;
 
- /* You can add/change/delete class attributes if you think it would be
-  * appropriate.
-  *
-  * You can also add helper methods and change the implementation of those
-  * provided if you think it would be appropriate, as long as you DO NOT
-  * CHANGE the interface.
-  */
+/* You can add/change/delete class attributes if you think it would be
+ * appropriate.
+ *
+ * You can also add helper methods and change the implementation of those
+ * provided if you think it would be appropriate, as long as you DO NOT
+ * CHANGE the interface.
+ */
 
 public class Sensor implements ISensor {
-    private float measurement;
 
-    private final static int max_measure = 50;
-    private final static int min_measure = 10;
+  private float measurement;
 
-    private DatagramSocket s;
-    private byte[] buffer;
+  private static final int max_measure = 50;
+  private static final int min_measure = 10;
 
-    /* Note: Could you discuss in one line of comment what you think can be
-     * an appropriate size for buffsize?
-     * (Which is used to init DatagramPacket?)
+  private DatagramSocket s;
+  private byte[] buffer;
+
+  /* Note: Could you discuss in one line of comment what you think can be
+   * an appropriate size for buffsize?
+   * (Which is used to init DatagramPacket?)
+   */
+  private static final int buffsize = 2048;
+
+  public Sensor(String address, int port, int totMsg) throws SocketException, UnknownHostException {
+    /* TODO: Build Sensor Object */
+    buffer = new byte[buffsize];
+    try {
+      InetAddress addr = InetAddress.getByName(address);
+      s = new DatagramSocket(port, addr);
+      System.out.println(s.getPort());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void run(int N) throws InterruptedException, IOException {
+    /* TODO: Send N measurements */
+    for (int i = 0; i < N; i++) {
+      measurement = this.getMeasurement();
+      MessageInfo messageInfo = new MessageInfo(N, i, measurement);
+      sendMessage(String.valueOf(s.getInetAddress()), s.getPort(), messageInfo);
+      System.out.println(
+          "[Sensor] Sending message " + i + " out of " + N + ". Measure = " + measurement);
+    }
+    /* Hint: You can get ONE measurement by calling
+     *
+     * float measurement = this.getMeasurement();
      */
-    private static final int buffsize = 2048;
 
-    public Sensor(String address, int port, int totMsg) {
-        /* TODO: Build Sensor Object */
+    /* TODO: Call sendMessage() to send the msg to destination */
+
+  }
+
+  public static void main(String[] args) throws InterruptedException, IOException {
+    if (args.length < 3) {
+      System.out.println("Usage: ./sensor.sh field_unit_address port number_of_measures");
+      return;
     }
 
-    @Override
-    public void run (int N) throws InterruptedException {
-        /* TODO: Send N measurements */
+    /* Parse input arguments */
+    String address = args[0];
+    int port = Integer.parseInt(args[1]);
+    int totMsg = Integer.parseInt(args[2]);
 
-        /* Hint: You can get ONE measurement by calling
-         *
-         * float measurement = this.getMeasurement();
-         */
+    /* TODO: Call constructor of sensor to build Sensor object*/
+    Sensor sensor = new Sensor(address, port, totMsg);
 
-         /* TODO: Call sendMessage() to send the msg to destination */
+    /* TODO: Use Run to send the messages */
+    sensor.run(totMsg);
+  }
 
-    }
+  @Override
+  public void sendMessage(String address, int port, MessageInfo msg) throws IOException {
+    String toSend = msg.toString();
 
-    public static void main (String[] args) {
-        if (args.length < 3) {
-            System.out.println("Usage: ./sensor.sh field_unit_address port number_of_measures");
-            return;
-        }
+    /* TODO: Build destination address object */
+    InetAddress addr = InetAddress.getByName(address);
 
-        /* Parse input arguments */
-        String address = args[0];
-        int port = Integer.parseInt(args[1]);
-        int totMsg = Integer.parseInt(args[2]);
+    /* TODO: Build datagram packet to send */
+    byte[] sendData = toSend.getBytes();
+    DatagramPacket p = new DatagramPacket(sendData, sendData.length, addr, port);
 
-        /* TODO: Call constructor of sensor to build Sensor object*/
+    /* TODO: Send packet */
+    s.send(p);
+  }
 
-        /* TODO: Use Run to send the messages */
+  @Override
+  public float getMeasurement() {
+    Random r = new Random();
+    measurement = r.nextFloat() * (max_measure - min_measure) + min_measure;
 
-    }
-
-    @Override
-    public void sendMessage (String address, int port, MessageInfo msg) {
-        String toSend = msg.toString();
-
-        /* TODO: Build destination address object */
-
-        /* TODO: Build datagram packet to send */
-
-        /* TODO: Send packet */
-
-    }
-
-    @Override
-    public float getMeasurement () {
-        Random r = new Random();
-        measurement = r.nextFloat() * (max_measure - min_measure) + min_measure;
-
-        return measurement;
-    }
+    return measurement;
+  }
 }
